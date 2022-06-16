a227f6f29f443fd5ebf7d0f66bc2428bede8e64e
cxycool <sodaforever@outlook.com>
2022-06-16 22:50:50 +0800

    update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ var _config = {
   nickname: "什么名字呢", //用户昵称
   repo: "moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
-  access_token: "ghp_vHI3gitgac7JDCkNH" + "68rTxdMDzCTxe0rWqSU", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+  access_token: "ghp_bNvk6M7FFED"+"5rIWJcMSJE7MQTHgOak1G0RUj", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
   per_page: 5, // 默认一页显示几篇文章
   state: "open" //文章状态 open closed all
 }
