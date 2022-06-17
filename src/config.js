babff92ff4844286d783035b4da97e2b2e499388
cxycool <sodaforever@outlook.com>
2022-06-17 10:45:50 +0800

    默认关闭好友动态

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ var _config = {
   nickname: "什么名字呢", //用户昵称
   repo: "moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
-  show_friend: true, //是否展示好友
+  show_friend: false, //是否展示好友
   friends_id: ["chunxiao01"], //好友id
   friends_name: ["凤梨罐头也过期"], //好友name
   access_token: "ghp_bNvk6M7FFED" + "5rIWJcMSJE7MQTHgOak1G0RUj", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
