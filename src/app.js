e3355c8f05ed951020733f9c270915f4aef65c99
cxycool <sodaforever@outlook.com>
2022-06-19 19:54:42 +0800

    update: 调整返回顶部按钮显示和隐藏的滚动高度

--- src/app.js
+++ src/app.js
@@ -329,7 +329,7 @@ const app = new Vue({
     //滚动内容实时监听位置
     scrollPosition(position) {
       const position_y = Math.abs(position.y)
-      this.isShowbacktop = position_y > 1200
+      this.isShowbacktop = position_y > 500
     },
     asideSetClick() {
       //设置
