f3835fc5de7da3ac94ed1da083bf744495aa5693
cxycool <sodaforever@outlook.com>
2022-06-19 11:33:47 +0800

    update:更新博客发布时间显示

--- src/app.js
+++ src/app.js
@@ -286,6 +286,8 @@ const app = new Vue({
         return "刚刚"
       } else if (diff_minute < 60) {
         return diff_minute + "分钟前"
+      } else if (diff_hour < 24) {
+        return diff_minute + "小时前"
       } else {
         return dayjs(date).format("YYYY-MM-DD HH:mm:ss")
       }
