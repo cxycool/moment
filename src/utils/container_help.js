6da968afdd18a51421d70aa56ad2fc592198b049
cxycool <sodaforever@outlook.com>
2022-06-25 19:41:23 +0800

    fix: 优化容器样式

--- src/utils/container_help.js
+++ src/utils/container_help.js
@@ -5,5 +5,5 @@ function get_container_height_init() {
   //重新计算container高度
   const clientHeight =
     document.body.clientHeight || document.documentElement.clientHeight
-  document.getElementById("container").style.height = clientHeight - 40 + "px"
+  document.getElementById("container").style.height = clientHeight - 55 + "px"
 }
