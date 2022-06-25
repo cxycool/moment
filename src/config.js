2bea0c2d4ad1c96de6dfc5e6f4a41cf9e30d2b2b
cxycool <sodaforever@outlook.com>
2022-06-25 19:32:45 +0800

    fix: 修改config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 var _config = {
-  blog_name: "一寸光阴", //"朝花夕拾•札记",//"you are my destiny", // 博客名称
+  blog_name: "我的随笔时光机", // 博客名称
   blog_url: "", //博客地址
   blog_pub_url: "", //博客发布地址
-  owner: "chunxiao01", // github 用户名
-  nickname: "鳳梨罐頭也過期", //"哆啦A梦", //用户昵称
-  repo: "my-moment", // github 中对应仓库名
+  owner: "cxycool", // github 用户名
+  nickname: "什么名字呢", //用户昵称
+  repo: "moment", // github 中对应仓库名
   blog_author_type: "OWNER", //发布博客的用户 默认 OWNER
   show_friend: false, //是否展示好友
   friends_id: [], //好友id
   friends_name: [], //好友name
   friends_access_token: [], //好友token
-  access_token: "ghp_z5x6Adj1yJRWAhCfeoB" + "2XvTohnyqUk21G61w", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+  access_token: "ghp_ztEpvo6pBrp7SVQ" + "8KfOZWXgSQTKdqf1GqkZ0", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
   per_page: 20, // 默认一页显示几篇文章
-  state: "open", //文章状态 open closed all
+  state: "open" //文章状态 open closed all
   github_url: "github.com",
   github_ip: "20.205.243.166"
 }
