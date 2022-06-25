7e7c04368295f4154463891ce47e5b2e5f717fdc
cxycool <sodaforever@outlook.com>
2022-06-25 19:34:35 +0800

    fix: 修改config.js

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ var _config = {
   friends_access_token: [], //好友token
   access_token: "ghp_ztEpvo6pBrp7SVQ" + "8KfOZWXgSQTKdqf1GqkZ0", // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
   per_page: 20, // 默认一页显示几篇文章
-  state: "open" //文章状态 open closed all
+  state: "open", //文章状态 open closed all
   github_url: "github.com",
   github_ip: "20.205.243.166"
 }
