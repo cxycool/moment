fd79e6ac5054dd2088e0e63238c46530184650c6
cxycool <107160391+cxycool@users.noreply.github.com>
2022-07-12 18:33:47 +0800

    fix: 修改issue模板

--- plugin/music/nest_music/src/nest_music.js
+++ plugin/music/nest_music/src/nest_music.js
@@ -1,6 +1,8 @@
 //获取链接参数
 function GetQueryString(name) {
   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)")
+  console.log(window.location)
+  console.log(window.location.search)
   var r = window.location.search.substr(1).match(reg)
   if (r != null) return decodeURI(r[2])
   return null
