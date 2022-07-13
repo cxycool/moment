dc1ad2fcbc176238fd3a0c85e6305b20f581e109
cxycool <107160391+cxycool@users.noreply.github.com>
2022-07-13 10:16:31 +0800

    feat: 新增QQ音乐播放插件

--- plugin/music/src/music.js
+++ plugin/music/src/music.js
@@ -9,10 +9,22 @@ function GetQueryString(name) {
 
 //获取歌曲信息
 const music_id = GetQueryString("id") //歌曲id
-
+const music_platform = GetQueryString("platform") //歌曲来源
 //音乐实例
-if (music_id) {
-  document.getElementById(
-    "music_container"
-  ).innerHTML = `<meting-js server="netease" type="song" id="${music_id}"></meting-js>`
+if (music_platform === "Q" || music_platform === "q") {
+  //QQ
+  if (music_id) {
+    document.getElementById("music_container").innerHTML = `<meting-js
+    auto="https://y.qq.com/n/yqq/song/${music_id}.html">
+  </meting-js>`
+  }
+} else if (music_platform === "N" || music_platform === "n") {
+  //网易云音乐
+  if (music_id) {
+    document.getElementById(
+      "music_container"
+    ).innerHTML = `<meting-js server="netease" type="song" id="${music_id}"></meting-js>`
+  }
+} else {
+  document.getElementById("music_container").innerHTML = "暂无歌曲"
 }
